cb152212c4dabd0fcf816a6b27171af365cf1bb1
Dan Kaiser <Tophatman100@gmail.com>
2019-02-06 14:13:32 -0600

    add baseline hello world in HTML and the render func in index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import react from 'react';
+import reactDOM from 'react-dom';
+
+class App extends extends ReactComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (<h1>Hello World!</h1>);
+  }
+}
+
