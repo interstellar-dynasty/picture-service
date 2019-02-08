67eca7943b2c831b5ebbb34756538e81377b787d
Dan Kaiser <Tophatman100@gmail.com>
2019-02-08 15:50:16 -0600

    pull data down from the S3 service and seed the database with said data

--- db/index.js
+++ db/index.js
@@ -1,4 +1,32 @@
 const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+/*
+Stack overflow example to access the bucket
+https://stackoverflow.com/questions/30726079/aws-s3-object-listing
+
+var AWS = require('aws-sdk');
+AWS.config.update({accessKeyId: 'mykey', secretAccessKey: 'mysecret', region: 'myregion'});
+var s3 = new AWS.S3();
+
+var params = { 
+ Bucket: 'mystore.in',
+ Delimiter: '/',
+ Prefix: 's/5469b2f5b4292d22522e84e0/ms.files/'
+}
+
+s3.listObjects(params, function (err, data) {
+ if(err)throw err;
+ console.log(data);
+});
+*/
+
+let pictureSchema = new Schema({
+  url: String,
+  title: String
+});
+
+let Picture = mongoose.model('Picture', pictureSchema);
 
 let db = mongoose.connect("mongodb://localhost:27017/picturesDB", { useNewUrlParser: true }, (err) => {
   if (err) {
@@ -7,7 +35,21 @@ let db = mongoose.connect("mongodb://localhost:27017/picturesDB", { useNewUrlPar
     console.log('connected to the mongo picturesDB database');
   }
 });
+function seedDatabase(url, title) {
+  let currSchema = new Picture({
+    url: url,
+    title: title
+  });
+  currSchema.save((err) => {
+    if (err) {
+      console.log(err, ' err in seedDatabase');
+    } else {
+      console.log(`saved ${currSchema} to the database`);
+    }
+  })
+}
 
 
 
+module.exports.seedDatabase = seedDatabase;
 module.exports.db = db;
---
+++ sample.js
@@ -0,0 +1,21 @@
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+let db = require('./db/index');
+
+let myBucket = new AWS.S3();
+let bucketName = 'picture-service-fec-bucket';
+myBucket.listObjects({ Bucket: 'picture-service-fec-bucket' }, (err, data) => {
+  if (err) {
+    console.log(err, 'you have an error in .listObjects');
+  } else {
+    // console.log(Array.isArray(data.Contents));
+    data.Contents.forEach(item => {
+      let currKey = item.Key;
+      let strTemplate = `https://${bucketName}.s3.amazonaws.com/${currKey}`;
+      console.log(item);
+      db.seedDatabase(strTemplate, currKey);
+      //add the currentTemplate to the database as the URL
+    });
+  }
+});
