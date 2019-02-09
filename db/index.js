602380c35428e2c718fa389007b0e154f1fe2274
Dan Kaiser <Tophatman100@gmail.com>
2019-02-09 15:08:38 -0600

    checkout to branch

--- db/index.js
+++ db/index.js
@@ -1,25 +1,6 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-/*
-Stack overflow example to access the bucket
-https://stackoverflow.com/questions/30726079/aws-s3-object-listing
-
-var AWS = require('aws-sdk');
-AWS.config.update({accessKeyId: 'mykey', secretAccessKey: 'mysecret', region: 'myregion'});
-var s3 = new AWS.S3();
-
-var params = { 
- Bucket: 'mystore.in',
- Delimiter: '/',
- Prefix: 's/5469b2f5b4292d22522e84e0/ms.files/'
-}
-
-s3.listObjects(params, function (err, data) {
- if(err)throw err;
- console.log(data);
-});
-*/
 
 let pictureSchema = new Schema({
   url: String,
@@ -49,6 +30,24 @@ function seedDatabase(url, title) {
   })
 }
 
-
 module.exports.seedDatabase = seedDatabase;
 module.exports.db = db;
+/*
+Stack overflow example to access the bucket
+https://stackoverflow.com/questions/30726079/aws-s3-object-listing
+
+var AWS = require('aws-sdk');
+AWS.config.update({accessKeyId: 'mykey', secretAccessKey: 'mysecret', region: 'myregion'});
+var s3 = new AWS.S3();
+
+var params = {
+ Bucket: 'mystore.in',
+ Delimiter: '/',
+ Prefix: 's/5469b2f5b4292d22522e84e0/ms.files/'
+}
+
+s3.listObjects(params, function (err, data) {
+ if(err)throw err;
+ console.log(data);
+});
+*/
